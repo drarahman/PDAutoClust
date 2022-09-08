9e861b5c51d01649ce1f70d710e5d394b0670bd4
Md Anisur Rahman <107666073+drarahman@users.noreply.github.com>
2022-09-08 23:07:15 +1000

    Update PDAutoClust.java

--- PDAutoClust/src/Techniques/PDAutoClust.java
+++ PDAutoClust/src/Techniques/PDAutoClust.java
@@ -22,7 +22,7 @@ import java.util.TreeSet;
  * School of Computing, Mathematics and Engineering, 
  * Charles Sturt University, Australia
  * Email: arahman@csu.edu.au; javedcse@gmail.com
- * date 17/06/2022
+ * Date: 17/06/2022
  */
 
 /**
